7ab1d64f302b23f6ef429f020756f4c447b469b3
Tonicrespit <crespi.toni@gmail.com>
2018-05-14 00:31:36 +0200

    Updated tests

--- test/HMM_test.py
+++ test/HMM_test.py
@@ -1,4 +1,5 @@
 from markovify.HMM import HMM
+from markovify.ViterbiDecoder import ViterbiDecoder
 
 
 def HMM_test():
@@ -6,6 +7,10 @@ def HMM_test():
     model.train('.', 'test.txt')
     print(model.a)
     print(model.b)
+
+    decoder = ViterbiDecoder(model)
+    result = decoder.viterbi(["Janet", "will", "back", "the", "bill"])
+    print(result)
 
 
 HMM_test()
