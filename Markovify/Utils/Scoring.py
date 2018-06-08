f7c01dc9f9c95d6938aa595800516773531e66f9
Tonicrespit <crespi.toni@gmail.com>
2018-06-08 13:55:54 +0200

    Updated metrics and examples

--- Markovify/Utils/Scoring.py
+++ Markovify/Utils/Scoring.py
@@ -6,49 +6,40 @@ from sklearn.preprocessing import MultiLabelBinarizer
 from .Utils import get_tags
 
 
-def confusion_matrix(true_tags, pred_tags, as_df=False):
-    if as_df:
-        tags = set(get_tags(true_tags, flatten=True)).union(set(get_tags(pred_tags, flatten=True)))
-        tags = tuple(tags)
+def confusion_matrix(true_tags, pred_tags, sample_weight=None):
+    true_tags, pred_tags, tags = _pre_process(true_tags, pred_tags, binarize=False)
 
-    true_tags, pred_tags = _pre_process(true_tags, pred_tags)
-
-    cm = sklearn.metrics.confusion_matrix(true_tags, pred_tags)
-    if not as_df:
-        return cm
-    else:
-        cm_df = pd.DataFrame(cm)
-        cm_df.columns = tags
-        cm_df['Index'] = tags
-        cm_df.set_index('Index', inplace=True)
-        return cm_df
+    return sklearn.metrics.confusion_matrix(true_tags, pred_tags, labels=tags, sample_weight=sample_weight)
 
 
-def accuracy(true_tags, pred_tags):
-    true_tags, pred_tags = _pre_process(true_tags, pred_tags)
+def accuracy(true_tags, pred_tags, normalize=True, sample_weight=None):
+    true_tags, pred_tags, tags = _pre_process(true_tags, pred_tags)
 
-    return sklearn.metrics.accuracy_score(true_tags, pred_tags)
-
-
-def precision(true_tags, pred_tags):
-    true_tags, pred_tags = _pre_process(true_tags, pred_tags)
-
-    return sklearn.metrics.precision_score(true_tags, pred_tags)
+    return sklearn.metrics.accuracy_score(true_tags, pred_tags, normalize, sample_weight)
 
 
-def recall(true_tags, pred_tags):
-    true_tags, pred_tags = _pre_process(true_tags, pred_tags)
+def precision(true_tags, pred_tags, average='macro', sample_weight=None):
+    true_tags, pred_tags, tags = _pre_process(true_tags, pred_tags, binarize=False)
 
-    return sklearn.metrics.recall_score(true_tags, pred_tags)
-
-
-def f_score(true_tags, pred_tags):
-    true_tags, pred_tags = _pre_process(true_tags, pred_tags)
-
-    return sklearn.metrics.f1_score(true_tags, pred_tags)
+    return sklearn.metrics.precision_score(true_tags, pred_tags,
+                                           labels=tags, average=average, sample_weight=sample_weight)
 
 
-def _pre_process(true_tags, pred_tags):
+def recall(true_tags, pred_tags, average='weighted', sample_weight=None):
+    true_tags, pred_tags, tags = _pre_process(true_tags, pred_tags, binarize=False)
+
+    return sklearn.metrics.recall_score(true_tags, pred_tags,
+                                        labels=tags, average=average, sample_weight=sample_weight)
+
+
+def f_score(true_tags, pred_tags, average='weighted', sample_weight=None):
+    true_tags, pred_tags, tags = _pre_process(true_tags, pred_tags, binarize=False)
+
+    return sklearn.metrics.f1_score(true_tags, pred_tags,
+                                    labels=tags, average=average, sample_weight=sample_weight)
+
+
+def _pre_process(true_tags, pred_tags, binarize=True):
     true_tags = get_tags(true_tags, flatten=True)
     pred_tags = get_tags(pred_tags, flatten=True)
 
@@ -58,8 +49,10 @@ def _pre_process(true_tags, pred_tags):
     tags = set(true_tags).union(set(pred_tags))
     tags = tuple(tags)
 
-    for i in range(0, len(true_tags)):
-        true_tags[i] = tags.index(true_tags[i])
-        pred_tags[i] = tags.index(pred_tags[i])
+    if binarize:
+        for i in range(0, len(true_tags)):
+            true_tags[i] = tags.index(true_tags[i])
+            pred_tags[i] = tags.index(pred_tags[i])
 
-    return true_tags, pred_tags
+    return true_tags, pred_tags, tags
+
