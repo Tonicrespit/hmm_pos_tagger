e65254683e794b2caf5f1bb2bcbb3f2acf2229a7
Tonicrespit <crespi.toni@gmail.com>
2018-09-16 17:44:47 +0200

    Renamed Markovify -> HMM. Added README

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# hmm_pos_tagger
+# Hidden Markov Model-based POS-tagger.
+
+# Setup.
+
+1. Install Python 3.6 or later. If you are not sure about the version you have installed, type `python -V` or `python3 -V` in your terminal.
+2. 
+
+# Running the included examples.
+There is a 
+
+# Quick start example.
+
+First of all you need to import the model wrapper that includes everything you need for training and decoding.
+
+```python
+from HMM.Markovify import *
+```
+
+For this guide we will use a corpora from the [NLTK library](https://github.com/nltk/nltk). If you want to use another corpora make sure it is in the correct format. The standard format for a text is having every word followed by '/TAG', for example:
+
+```I/pronoun am/verb a/preposition correctly/adjective tagged/verb text/noun ./.```
+
+The model is tag-agnostic, so you can use any tagset. If you are not sure about what tagset to use, it is recommended to use the universal tagset.
+
+```python
+from nltk.corpus import brown
+
+corpus = brown.tagged_sents(categories='news', tagset='universal')
+```
+
+The NLTK library provides the corpora as a list of sentences, each sentence represented by a list of words. Each word is a tuple (word, tag). For example:
+
+```python
+[[("Sentence", "NOUN"), ("1", "Number")],
+ [("Sentence", "NOUN"), ("2", "Number")],
+ ...
+]
+```
+
+To create the model, first we call the HMM constructor with the tunning parameters:
+
+```python
+model = Markovify(smoothing='laplace', alpha=1)
+```
+
+For this example we will use a default Hidden Markov Model with Laplace (or add-one) smoothing.
+
+To train the model you just have to call the function `fit` with the corpus. There are multiple ways of specifying the training corpus, check the documentation to see what fits your needs better.
+
+```python
+model = model.fit(sentences=corpus)
+```
+
+Once the model is trained, we create a decoder:
+
+```python
+decoder = model.predict(["I", "went", "to", "school", "yesterday", "."])
+```
+
+Do you want more examples? Please check the included Jupyter notebook!
--- test/HMM_test.py
+++ test/HMM_test.py
@@ -1,16 +1,70 @@
-from markovify.markovify import markovify
-from markovify.utils.CrossValidation import cross_val_score
-
+import numpy as np
 from nltk.corpus import brown
 
-
-def HMM_test():
-    corpus = brown.tagged_sents(categories='news', tagset='universal')
-    # mini_corpus = list(corpus[i] for i in range(0, 320))
-
-    m = markovify()
-    result = cross_val_score(m, sentences=corpus, verbose=True)
-    print(result)
+from HMM.Markovify import *
+from HMM.Utils.CrossValidation import cross_validation, cross_validation_score
+from HMM.Utils.Scoring import *
 
 
-HMM_test()
+def hmm_test():
+    corpus = brown.tagged_sents(categories='adventure', tagset='universal')
+    corpus_news = brown.tagged_sents(categories='news', tagset='universal')
+    corpus_religion = brown.tagged_sents(categories='religion', tagset='universal')
+    corpus_romance = brown.tagged_sents(categories='romance', tagset='universal')
+    # mini_corpus = list(corpus[i] for i in range(0, 200))
+
+    m_max = Markovify(smoothing='max')
+    acc, m_max = cross_validation_score(m_max, sentences=corpus, verbose=False, cv=10, return_model=True)
+    print(acc[np.argmax([i['accuracy'] for i in acc])])
+
+    predicted_corpus = m_max.predict(corpus_news)
+    print({'accuracy': accuracy(corpus_news, predicted_corpus),
+           'precision': precision(corpus_news, predicted_corpus),
+           'recall': recall(corpus_news, predicted_corpus)})
+
+    predicted_corpus = m_max.predict(corpus_religion)
+    print({'accuracy': accuracy(corpus_religion, predicted_corpus),
+           'precision': precision(corpus_religion, predicted_corpus),
+           'recall': recall(corpus_religion, predicted_corpus)})
+
+    predicted_corpus = m_max.predict(corpus_romance)
+    print({'accuracy': accuracy(corpus_romance, predicted_corpus),
+           'precision': precision(corpus_romance, predicted_corpus),
+           'recall': recall(corpus_romance, predicted_corpus)})
+
+    # m_laplace = Markovify(smoothing='laplace', alpha=1)
+    # scores_laplace = cross_validation_score(m_laplace, sentences=corpus, verbose=False, cv=10)
+    # print(scores_laplace)
+
+    # amax_max = np.argmax([i['accuracy'] for i in scores_max])
+    # alaplace_max = np.argmax([i['accuracy'] for i in scores_laplace])
+
+    # print(scores_max[amax_max])
+    # print(scores_laplace[alaplace_max])
+
+    # m = m.fit(mini_corpus)
+    # pred = m.predict(mini_corpus)
+    # print(confusion_matrix(mini_corpus, pred))
+    # print(m.predict(mini_corpus))
+    # print(m.predict([['The', 'black'], ['man', 'is']]))
+
+    # corpus = brown.tagged_sents(categories='news', tagset='universal')
+
+    # model_laplace = Markovify(smoothing='laplace', alpha=1)
+    # start = time.time()
+    # brown_news_accuracy_laplace = cross_validation_score(model_laplace, sentences=corpus, verbose=True)
+    # end = time.time()
+    # print('Time elpased: {}s.'.format(end - start))
+    # print('Best accuracy for Laplace smoothing: {}.'.format(np.max(brown_news_accuracy_laplace)))
+    # tagged = model.predict(['The', 'black', 'man'])
+    # print(result)
+    # print(tagged)
+    # print(confusion_matrix(corpus, corpus))
+    # print(accuracy(corpus, corpus))
+    # print(recall(corpus, corpus))
+    # print(precision(corpus, corpus))
+    # print(f_score(corpus, corpus))
+
+
+if __name__ == '__main__':
+    hmm_test()
